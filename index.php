d0fa48600fa5e441c8af368a96df9d9088b4595b
Kleberus <kleber_lsgermano@hotmail.com>
2019-08-11 06:34:27 -0300

    Basic structure

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Test With Alternative Questions</title>
+	<meta name="description" content="The HTML5 Herald">
+<meta name="author" content="SitePoint">
+ <link rel="stylesheet" href="css/styles.css?v=1.0">
+
+	</head>
+	<body>
+		<h1>Prova com Questões Alternativas</h1>
+
+<form method='POST' action='result.php'>
+  <div id='1'></>
+  <main>
+  	
+ 
+<div class='bg_questions'>
+<p class='question'><span>01-</span> Em que ano se iniciou a Primeira Guerra Mundial?</p>
+
+  <p><span>(a)</span><input type="radio" name="question_01" value="28_junho_1914"> 28 de junho de 1914</p>
+    <p><span>(b)</span> <input type="radio" name="question_01" value="28_julho_1945"> 28 de julho de 1945</p>
+   <p> <span>(c)</span> <input type="radio" name="question_01" value="01_setembro_1939">01 de setembro de 1939</p>
+</div>
+
+<div class='bg_questions'>
+<p class='question'><span>02-</span> Quando teve início a Revolução Francesa?</p>
+  <p><span>(a)</span><input type="radio" name="question_01" value="19_outubro_1775">19 de outubro de 1775</p>
+  <p><span>(b)</span><input type="radio" name="question_01" value="05_maio_1789"> 05 de maio de 1789</p>
+ <p> <span>(c)</span><input type="radio" name="question_01" value="09_novembro_1799"> 09 de novembro de 1799</p>
+</div>
+
+<div class='bg_questions'>
+<p class='question'><span>03-</span> Quando teve início a Revolução Russa?</p>
+  <span>(a)</span><input type="radio" name="question_01" value="27_marco_1927">27 de março de 1927</p>
+  <span>(b)</span><input type="radio" name="question_01" value="08_marco_1917">08 de março de 1917</p>
+  <span>(c)</span><input type="radio" name="question_01" value="07_maio_1914">07 de maio de 1914</p>
+</div>
+
+
+</form>
+
+ </main>
+
+
+	</body>
+
+</html>
